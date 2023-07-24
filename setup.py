fb48414a444759e33f3811575b08037a416587ad
GoncaloGuedes_CorkSupply <dp@corksupply.pt>
2023-07-24 12:16:04 +0100

    add requirements.txt

--- setup.py
+++ setup.py
@@ -3,12 +3,15 @@ from setuptools import find_packages, setup
 with open("./README.md", "r") as f:
     long_description = f.read()
 
+# Read requirements.txt and extract dependencies
+with open("requirements.txt", "r") as req_file:
+    requirements = req_file.read().splitlines()
+
 setup(
     name="chemopy",
     version="0.0.10",
     description="An package that contains preprocessing, model transfer and orthogonal corrections for Spectroscopy",
-    package_dir={"": "chemopy"},
-    packages=find_packages(where="chemopy"),
+    packages=find_packages(where="."),
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Gonçalo Guedes",
@@ -20,4 +23,5 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.10",
+    install_requires=requirements,  # Use the extracted dependencies from requirements.txt
 )
