f802054d48d0dbb08deea2aa60ea56dd7208ed18
goncaloguedes <goncalo.manuelguedes@gmail.com>
2024-03-15 19:24:48 +0000

    docs: Add Docstring to plots

--- chemopy/plots/_regression.py
+++ chemopy/plots/_regression.py
@@ -5,9 +5,24 @@ from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import cross_val_predict
 
 
-def plot_cv_regression(estimator: BaseEstimator, X_train, y_train, title=None) -> None:
+def plot_cv_regression(
+    estimator: BaseEstimator, X_train: np.ndarray, y_train: np.ndarray, title: str = ""
+) -> None:
+    """Plot the cross-validated predictions against the actual values and the calibration predictions.
+
+    Parameters
+    ----------
+    estimator : BaseEstimator
+        The estimator that was fitted using GridSearchCV or RandomizedSearchCV
+    X_train : np.ndarray
+        The training set
+    y_train : np.ndarray
+        The training set labels
+    title : str, optional
+        title of the plot, by default None
+    """
     # Predict on the test set using the best estimator found by GridSearchCV
-    y_pred = estimator.predict(X_train)
+    y_pred = estimator.predict(X_train)  # type:ignore
 
     # Compute R2 and RMSECV on the test set
     r2 = r2_score(y_train, y_pred)
@@ -15,23 +30,24 @@ def plot_cv_regression(estimator: BaseEstimator, X_train, y_train, title=None) -
 
     # Compute CV predictions on the training set
     cv_pred = cross_val_predict(
-        estimator.best_estimator_, X_train, y_train, cv=estimator.n_splits_
+        estimator.best_estimator_, # type:ignore
+        X_train,
+        y_train,
+        cv=estimator.n_splits_,  # type:ignore
     )
 
     # Compute R2 and RMSECV on the CV predictions
     r2cv = r2_score(y_train, cv_pred)
-    rmse_cv = -estimator.best_score_
+    rmse_cv = -estimator.best_score_  # type:ignore
 
     # Plot CV vs. actual values
     plt.scatter(y_train, cv_pred, alpha=0.5)
     plt.scatter(y_train, y_pred, alpha=0.5)
-    plt.plot([min(y_train), max(y_train)], [
-             min(y_train), max(y_train)], "k--", lw=2)
+    plt.plot([min(y_train), max(y_train)], [min(y_train), max(y_train)], "k--", lw=2)
     plt.xlabel("Actual Values")
     plt.ylabel("CV Predictions")
     plt.title(f"{title} CV vs. Actual Values")
-    plt.legend(
-        ["CV predictions", "Calibration predictions", "Perfect prediction"])
+    plt.legend(["CV predictions", "Calibration predictions", "Perfect prediction"])
 
     # Format R2 and RMSECV with three decimal points
     r2_str = f"{r2:.3f}"
@@ -46,16 +62,31 @@ def plot_cv_regression(estimator: BaseEstimator, X_train, y_train, title=None) -
     plt.show()
 
 
-def plot_predictions_regression(estimator: BaseEstimator, X, y, title=None):
+def plot_predictions_regression(
+    estimator: BaseEstimator, X: np.ndarray, y: np.ndarray, title: str = ""
+):
+    """Plot the predictions against the actual values.
+
+    Parameters
+    ----------
+    estimator : BaseEstimator
+        The estimator that was fitted using GridSearchCV or RandomizedSearchCV
+    X : np.ndarray
+        The test set
+    y : np.ndarray
+        The test set labels
+    title : str, optional
+        title of the plot, by default None
+    """
     # Predict on the test set using the best estimator found by GridSearchCV
-    y_pred = estimator.predict(X)
+    y_pred = estimator.predict(X)  # type:ignore
 
     # Compute R2 and RMSECV on the test set
     r2_pred = r2_score(y, y_pred)
     rmse_pred = np.sqrt(mean_squared_error(y, y_pred))
 
     # Compute CV predictions on the training set
-    rmse_cv = -estimator.best_score_
+    rmse_cv = -estimator.best_score_  # type:ignore
 
     # Plot CV vs. actual values
     plt.scatter(y, y_pred, alpha=0.5)
